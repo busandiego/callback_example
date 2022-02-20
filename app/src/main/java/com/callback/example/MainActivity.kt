df7146d00e889887130269628690ab03943ff13f
suin lee <silee@weedscomm.com>
2022-02-21 00:34:52 +0900

    comment add

--- app/src/main/java/com/callback/example/MainActivity.kt
+++ app/src/main/java/com/callback/example/MainActivity.kt
@@ -16,6 +16,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val button = findViewById<Button>(R.id.button)
         // 리스너란?
         // 뷰에서 발생하는 이벤트에 대해 작업을 처리해주는 곳.
+        
+        // TODO 명심해야할 부분
+        // 리스너를 구현한 쪽에서는(UI Controller) 이벤트이후 메소드의 구체적인 처리방법을 제시함(정의)
+        // 그리고 리스너를 받은 곳에서 (주로 직접적인 뷰) 구체적인 메소드를 호출
+        // 익명함수는 파라미터를 익명함수로 전달해서 바로 호출하는 거 (보통 뷰에 바인딩)
 
         // Activity에 Interface 구현
         // setOnClickListener에 리스너를 전달해서 보내야되는데
@@ -48,6 +53,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     }
 
+    // https://todaycode.tistory.com/24 콜백예시
 
     // Activity에 OnclickListener Interface 구현
     // 이벤트 발생시 수행되는 작업을 정의 함
